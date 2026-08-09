db570c3eaf1e788f04160c2777925855c86840e9
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-08-08 22:28:34 -0400

    Bump Talevane to v0.6.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "app.talevane.reader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.6.2"
+        versionCode = 9
+        versionName = "0.6.3"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
