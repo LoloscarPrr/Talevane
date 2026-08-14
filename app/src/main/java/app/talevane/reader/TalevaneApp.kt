09a40dd2ab88d32844b36f83fe55e29b5f76a761
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-08-14 19:59:10 -0400

    Provide narration gateway from application

--- app/src/main/java/app/talevane/reader/TalevaneApp.kt
+++ app/src/main/java/app/talevane/reader/TalevaneApp.kt
@@ -1,18 +1,24 @@
 package app.talevane.reader
 
 import android.app.Application
+import app.talevane.reader.application.narration.NarrationGateway
 import app.talevane.reader.data.BookRepository
 import app.talevane.reader.data.RoomBookRepository
 import app.talevane.reader.data.TalevaneDatabase
+import app.talevane.reader.speech.AndroidNarrationGateway
 import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 
 class TalevaneApp : Application() {
     lateinit var repository: BookRepository
         private set
 
+    lateinit var narrationGateway: NarrationGateway
+        private set
+
     override fun onCreate() {
         super.onCreate()
         PDFBoxResourceLoader.init(applicationContext)
         repository = RoomBookRepository(this, TalevaneDatabase.get(this).bookDao())
+        narrationGateway = AndroidNarrationGateway(this)
     }
 }
