d1e691d54c6760e946f4b2d7db4ff7cfe1d3be0b
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-08-14 06:02:47 -0400

    Wire repository implementation behind BookLibrary contract

--- app/src/main/java/app/talevane/reader/TalevaneApp.kt
+++ app/src/main/java/app/talevane/reader/TalevaneApp.kt
@@ -1,16 +1,18 @@
 package app.talevane.reader
 
 import android.app.Application
-import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
-import app.talevane.reader.data.TalevaneDatabase
 import app.talevane.reader.data.BookRepository
+import app.talevane.reader.data.RoomBookRepository
+import app.talevane.reader.data.TalevaneDatabase
+import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 
 class TalevaneApp : Application() {
     lateinit var repository: BookRepository
         private set
+
     override fun onCreate() {
         super.onCreate()
         PDFBoxResourceLoader.init(applicationContext)
-        repository = BookRepository(this, TalevaneDatabase.get(this).bookDao())
+        repository = RoomBookRepository(this, TalevaneDatabase.get(this).bookDao())
     }
 }
