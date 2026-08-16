612354ae322b743e2308df904545fcb095a459bd
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-08-15 22:25:03 -0400

    Bump Talevane to 0.7.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "app.talevane.reader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 35
-        versionName = "0.7.6"
+        versionCode = 36
+        versionName = "0.7.7"
     }
 
     if (hasStableKeystore) {
